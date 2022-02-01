fe4bcf2f4dccbff228fbad42b76eca52d71b589e
Denis Kuznetsov <detrevile3@gmail.com>
2022-02-01 16:42:17 +0300

    Adaptation slider and catalog complete

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,13 @@ $(document).ready(function () {
     $('.carousel__inner').slick({ // Slider script
         speed: 1000,
         // adaptiveHeight: true,
+        /*         autoplay: true,
+                autoplaySpeed: 2000, */
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider_left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/slider_right.svg"></button>',
         responsive: [
             {
-                breakpoint: 992,
+                breakpoint: 1130,
                 settings: {
                     dots: true,
                     arrows: false
