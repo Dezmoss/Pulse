1fa4203d4e1d537e20e46fab34d4437514379662
Denis Kuznetsov <detrevile3@gmail.com>
2022-02-01 18:06:17 +0300

    Footer section completed

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
     $('.carousel__inner').slick({ // Slider script
         speed: 1000,
         // adaptiveHeight: true,
-        /*         autoplay: true,
-                autoplaySpeed: 2000, */
+        autoplay: true,
+        autoplaySpeed: 2000,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider_left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/slider_right.svg"></button>',
         responsive: [
